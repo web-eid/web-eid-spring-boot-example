bc8e05f44cdb6d9f01dd264c865983e12afa0f6f
Mart Somermaa <mrts@users.noreply.github.com>
2022-07-26 17:45:02 +0300

    deps: roll back DigiDoc4j to version 4.3.0

--- scripts/deployment/fab.sh
+++ scripts/deployment/fab.sh
@@ -2,6 +2,8 @@
 
 set -eu
 
+cd "$( dirname "$0" )"
+
 SERVER_ADDRESS=web-eid.eu
 SERVER_USER=baron
 SERVER_PORT=22
@@ -9,4 +11,5 @@ SERVER_PORT=22
 export WEBEID_DIR='web-eid/web-eid-springboot-example'
 export WEBEID_DIR='web-eid/test-web-eid-springboot-example'
 
+. venv/bin/activate
 fab -e -H ${SERVER_USER}@${SERVER_ADDRESS}:${SERVER_PORT} "$@"
