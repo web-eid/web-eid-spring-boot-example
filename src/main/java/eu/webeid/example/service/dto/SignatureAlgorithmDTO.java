48818d5974adc4d03a60fdfeb99261a46de52754
Mart Somermaa <mrts@users.noreply.github.com>
2023-09-07 14:55:38 +0300

    feat: validate signature algorithm values
    WE2-817
    
    Signed-off-by: Mart Somermaa <mrts@users.noreply.github.com>

--- src/main/java/eu/webeid/example/service/dto/SignatureAlgorithmDTO.java
+++ src/main/java/eu/webeid/example/service/dto/SignatureAlgorithmDTO.java
@@ -22,7 +22,24 @@
 
 package eu.webeid.example.service.dto;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class SignatureAlgorithmDTO {
+
+    // See https://github.com/web-eid/web-eid-app/blob/main/src/controller/command-handlers/signauthutils.cpp#L121-L127
+    private static final Set<String> SUPPORTED_CRYPTO_ALGOS = new HashSet<>(Arrays.asList(
+            "ECC", "RSA"
+    ));
+    private static final Set<String> SUPPORTED_PADDING_SCHEMES = new HashSet<>(Arrays.asList(
+            "NONE", "PKCS1.5", "PSS"
+    ));
+    // See https://github.com/web-eid/libelectronic-id/tree/main/src/electronic-id.cpp#L131
+    private static final Set<String> SUPPORTED_HASH_FUNCTIONS = new HashSet<>(Arrays.asList(
+            "SHA-224", "SHA-256", "SHA-384", "SHA-512",
+            "SHA3-224", "SHA3-256", "SHA3-384", "SHA3-512"
+    ));
 
     private String cryptoAlgorithm;
 
@@ -35,6 +52,9 @@ public class SignatureAlgorithmDTO {
     }
 
     public void setCryptoAlgorithm(String cryptoAlgorithm) {
+        if (!SUPPORTED_CRYPTO_ALGOS.contains(cryptoAlgorithm)) {
+            throw new IllegalArgumentException("The provided crypto algorithm is not supported");
+        }
         this.cryptoAlgorithm = cryptoAlgorithm;
     }
 
@@ -43,6 +63,9 @@ public class SignatureAlgorithmDTO {
     }
 
     public void setHashFunction(String hashFunction) {
+        if (!SUPPORTED_HASH_FUNCTIONS.contains(hashFunction)) {
+            throw new IllegalArgumentException("The provided hash function is not supported");
+        }
         this.hashFunction = hashFunction;
     }
 
@@ -51,6 +74,9 @@ public class SignatureAlgorithmDTO {
     }
 
     public void setPaddingScheme(String paddingScheme) {
+        if (!SUPPORTED_PADDING_SCHEMES.contains(paddingScheme)) {
+            throw new IllegalArgumentException("The provided padding scheme is not supported");
+        }
         this.paddingScheme = paddingScheme;
     }
 }
