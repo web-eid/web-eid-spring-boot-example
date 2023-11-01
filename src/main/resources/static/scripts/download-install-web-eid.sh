64a5f17ba9b1264f502a697ee99ff55eb0eb916b
Kristel Merilain <kristel.merilain@ria.ee>
2023-11-01 15:17:17 +0200

    Add mantic support
    Signed-off-by: Kristel Merilain <kristel.merilain@ria.ee>

--- src/main/resources/static/scripts/download-install-web-eid.sh
+++ src/main/resources/static/scripts/download-install-web-eid.sh
@@ -62,8 +62,9 @@ test_sudo
 # 20.04     focal   LTS   2025-04
 # 22.04     jammy   LTS   2027-04
 # 23.04     lunar   -     2024-01
-LATEST_SUPPORTED_UBUNTU_CODENAME='lunar'
-LATEST_SUPPORTED_UBUNTU_VERSION='23.04'
+# 23.10     mantic   -     2024-07
+LATEST_SUPPORTED_UBUNTU_CODENAME='mantic'
+LATEST_SUPPORTED_UBUNTU_VERSION='23.10'
 
 # Check the distro and release.
 distro=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
@@ -98,7 +99,7 @@ case $distro in
         utopic|vivid|wily|trusty|artful|cosmic|disco|xenial|eoan|groovy|hirsute|impish|bionic|kinetic)
           make_fail "Ubuntu $codename is not officially supported"
           ;;
-        focal|jammy|lunar)
+        focal|jammy|lunar|mantic)
           make_install $release
           ;;
         *)
