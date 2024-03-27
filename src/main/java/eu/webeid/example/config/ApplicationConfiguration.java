d68f1c8355f6605be39608a3fff82c2d66a9c5cd
Lauris Kaplinski <lauris@raulwalter.com>
2024-03-27 13:27:21 +0200

    Fixed Java version
    Signed-off-by: Lauris Kaplinski <lauris@raulwalter.com>

--- src/main/java/eu/webeid/example/config/ApplicationConfiguration.java
+++ src/main/java/eu/webeid/example/config/ApplicationConfiguration.java
@@ -31,7 +31,6 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
